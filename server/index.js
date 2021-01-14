798c95d763badc7c3dddd7069bf257b80688a46b
Laura Bhayani <14044896+laurabeth@users.noreply.github.com>
2021-01-14 18:13:25 -0500

    create server, add redirect, install react router

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.use((req, res, next) => {
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server no listening at http://localhost:${port}`);
+});
